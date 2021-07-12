73fbbc09f107f0d090e301dcc73e847b060776fb
efrain <chernanf@everis.com>
2021-07-12 18:38:32 -0500

    204 no content

--- src/main/java/com/subcon/app/restcontroller/UsuariosController.java
+++ src/main/java/com/subcon/app/restcontroller/UsuariosController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,14 +21,32 @@ public class UsuariosController {
 	private UsuariosRepository repositoryUsuarios;
 
 	@PostMapping(path = "/usuario", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public Usuario add(@RequestBody Usuario usuario) {
-		return repositoryUsuarios.save(usuario);
+	public ResponseEntity<Usuario> add(@RequestBody Usuario usuario) {
+		Usuario user = repositoryUsuarios.save(usuario);
+		if(user == null)
+		{
+			return ResponseEntity.status(500).body(user);
+		}
+		else
+		{
+			return ResponseEntity.status(200).body(user);
+		}
 	}
 
 	@GetMapping(path = "/usuario", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Usuario> login(@RequestHeader(name = "usuario", required = true) String usuario, @RequestHeader(name = "password", required = true) String password) {
+	public ResponseEntity<List<Usuario>> login(@RequestHeader(name = "usuario", required = true) String usuario, @RequestHeader(name = "password", required = true) String password) {
 		
-		return repositoryUsuarios.login(usuario, password);
+		List<Usuario> list = repositoryUsuarios.login(usuario, password);
+	
+		if(list == null || list.isEmpty())
+		{
+			return ResponseEntity.status(204).body(list);
+		}
+		else
+		{
+			return ResponseEntity.status(200).body(list);
+		}
+		
 	}
 
 
